4abfe5e987dd5334d8d7da7482ab00b67f8836e1
bbenny95 <Bbenny6347@conestogac.on.ca>
2022-07-13 17:55:13 -0400

    framework component

--- app_ui/src/app/app.module.ts
+++ app_ui/src/app/app.module.ts
@@ -3,15 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HomeComponent } from './home/home.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { FrameworkComponent } from './framework/framework.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    FrameworkComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      },
+      
+    ])
+
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
-  bootstrap: [HomeComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
