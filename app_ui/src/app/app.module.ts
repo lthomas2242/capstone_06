23073cbb60fc5065874132d88ebcb0e48a559858
LibyThomas <lthomas2242@conestogac.on.ca>
2022-07-18 19:25:43 -0400

    restructure components and layouts

--- app_ui/src/app/app.module.ts
+++ app_ui/src/app/app.module.ts
@@ -1,28 +1,69 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { HomeComponent } from './home/home.component';
 import { APP_BASE_HREF } from '@angular/common';
-import { FrameworkComponent } from './framework/framework.component';
 import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
+import {MaterialModule} from './material.module';
+
+import { HomeComponent } from './_components/home/home.component';
+import { FrameworkComponent } from './_layouts/framework/framework.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './_components/login/login.component';
+import { CustomerLayoutComponent } from './_layouts/customer-layout/customer-layout.component';
+import { AdminLayoutComponent } from './_layouts/admin-layout/admin-layout.component';
+import { LandingPageComponent } from './_components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    FrameworkComponent
+    FrameworkComponent,
+    AppComponent,
+    LoginComponent,
+    CustomerLayoutComponent,
+    AdminLayoutComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MaterialModule,
     RouterModule.forRoot([
       {
         path: '',
-        component: HomeComponent
+        component: LandingPageComponent
       },
-      
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'cust-layout',
+        component: CustomerLayoutComponent,
+        children:[
+          {
+            path: '',
+            component: HomeComponent
+          }
+        ]
+      },
+      {
+        path: 'admin-layout',
+        component: AdminLayoutComponent,
+        children:[
+          {
+            path: '',
+            component: HomeComponent
+          }
+        ]
+      },
     ])
 
   ],
   providers: [{provide: APP_BASE_HREF, useValue: '/'}],
-  bootstrap: [FrameworkComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
