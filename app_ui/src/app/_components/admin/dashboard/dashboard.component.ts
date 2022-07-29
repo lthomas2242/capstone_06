6b6cbd3f770411997374c4e7d161982191ad04d5
LibyThomas <lthomas2242@conestogac.on.ca>
2022-07-29 02:49:06 -0400

    admin dashboard apis

--- app_ui/src/app/_components/admin/dashboard/dashboard.component.ts
+++ app_ui/src/app/_components/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RecipeService } from 'src/app/_services/recipe.service';
+import { UserService } from 'src/app/_services/user.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  public allRecipesCount = 0;
+  public approvedRecipesCount = 0;
+  public allUsersCount = 0;
+  constructor(private recipeService : RecipeService,private userService : UserService) { }
 
   ngOnInit(): void {
+    this.getAllRecipesCount();
+    this.getApprovedRecipesCount();
+    this.getAllUserCount();
   }
 
+  getAllRecipesCount(){
+      this.recipeService.getAllRecipesCount()
+      .subscribe({
+        next: (v) => {
+          this.allRecipesCount = v;
+        },
+    });
+  }
+
+  getApprovedRecipesCount(){
+      this.recipeService.getApprovedRecipesCount()
+      .subscribe({
+        next: (v) => {
+          this.approvedRecipesCount = v;
+        },
+    });
+  }
+
+  getAllUserCount(){
+    this.userService.getAllUsersCount()
+    .subscribe({
+      next: (v) => {
+        this.allUsersCount = v;
+      },
+  });
+}
 }
