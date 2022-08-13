b76951cd3e684f862619094a9a31bd644cb97eeb
vvenugopal1714 <vvenugopal1714@conestogac.on.ca>
2022-08-13 10:19:35 -0400

    hosting

--- app_ui/server.js
+++ app_ui/server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/capstone_06'));
+app.use(express.static('./dist/capstone_06'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/capstone_06/login'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/capstone_06/'}),
+);
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
